0742f3b134afc543c0e717c380c9f9c828fee1eb
xiaohao0576 <xiaohao0576@gmail.com>
2026-05-25 12:40:58 +0700

    POS产品列表，名字后面显示价格

---
+++ addons/pos_custom_ui/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    "name": "POS Custom UI",
+    "summary": "Show product name with sale price on POS product cards",
+    "version": "1.0",
+    "category": "Sales/Point of Sale",
+    "license": "LGPL-3",
+    "depends": ["point_of_sale"],
+    "assets": {
+        "point_of_sale._assets_pos": [
+            "pos_custom_ui/static/src/**/*",
+        ],
+    },
+    "installable": True,
+    "application": False,
+}

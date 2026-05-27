3e23edb668260a138fd271f2874600bd76b3d761
xiaohao0576 <xiaohao0576@gmail.com>
2026-05-27 08:41:40 +0700

    优化价格显示算法

--- addons/pos_custom_ui/static/src/app/screens/product_screen_patch.js
+++ addons/pos_custom_ui/static/src/app/screens/product_screen_patch.js
@@ -4,48 +4,10 @@ import { ProductScreen } from "@point_of_sale/app/screens/product_screen/product
 patch(ProductScreen.prototype, {
     getProductName(product) {
         const baseName = super.getProductName(...arguments) || product?.name || "";
-
-        try {
-            const currentOrder = this.currentOrder;
-            const pricelist = currentOrder?.pricelist_id || this.pos?.config?.pricelist_id;
-
-            if (!product?.getPrice) {
-                return baseName;
-            }
-
-            // Pricelist can be missing in some UI states; getPrice(false, ...) falls back to base sale price.
-            const basePrice = product.getPrice(pricelist || false, 1, 0, false, false);
-            const fiscalPosition = currentOrder?.fiscal_position_id || false;
-            const taxesData = product.getTaxDetails?.({
-                overridedValues: {
-                    price: basePrice,
-                    fiscalPosition,
-                },
-            });
-            const displayPrice =
-                taxesData && this.pos?.config?.iface_tax_included === "total"
-                    ? taxesData.total_included
-                    : taxesData?.total_excluded ?? basePrice;
-
-            if (!Number.isFinite(displayPrice)) {
-                console.warn(`${LOG_PREFIX} getProductName fallback: displayPrice is not finite`, {
-                    productId: product?.id,
-                    basePrice,
-                    displayPrice,
-                    taxesData,
-                });
-                return baseName;
-            }
-
-            const formatCurrency = this?.env?.utils?.formatCurrency;
-            if (!formatCurrency) {
-                return baseName;
-            }
-
-            const formattedPrice = formatCurrency(displayPrice);
-            return `${baseName}(${formattedPrice})`;
-        } catch {
+        const price = product?.displayPriceUnit;
+        if (!price) {
             return baseName;
         }
+        return `${baseName} | ${price}`;
     },
 });
